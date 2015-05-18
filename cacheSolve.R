693682ff69752309949532131a7db1b4e1862a75
dataNstories <dataNstories@gmail.com>
2015-05-18 12:35:08 -0700

    Commit new R scripts for programming assignment 2

---
+++ cacheSolve.R
@@ -0,0 +1,19 @@
+cacheSolve <- function(x=matrix(), ...) {
+  
+  m<-x$getmatrix()  #  retrieve the matrix from cache
+  
+ # testing if the matrix already exist?  
+ 
+  if(!is.null(m))
+  {
+    message("getting cached data")
+    return(m)  # retrun the matrix read from cache and exit the funcation
+  }
+
+ # if the inverse is not calculated, calcuate the inverse and return the matrix
+ 
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  return(m)
+}
---
+++ makeCacheMatrix.R
@@ -0,0 +1,20 @@
+makeCacheMatrix <- function(x = matrix()) {
+ 
+  m<-NULL  # initialize a matrix 
+    
+  set<-function(y){ # set the matrix 
+    x<<-y
+    m<<-NULL
+  }
+  # display the matrix
+  
+  get<-function() x
+  
+  setmatrix<-function(solve) m<<- solve  # calcuate the inverse and store back
+  getmatrix<-function() m  # retrieve the matrix 
+  
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
+  
+}
